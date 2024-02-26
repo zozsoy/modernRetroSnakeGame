4f2ef0bb223fbd0e156b49a5cd10d971a02a4bb6
ziya.ozsoy <zozsoy95@gmail.com>
2024-02-26 22:52:16 +0300

    Boş GLWindow oluşturuldu

---
+++ src/main/java/Screen.java
@@ -0,0 +1,49 @@
+import com.jogamp.newt.event.WindowAdapter;
+import com.jogamp.newt.event.WindowEvent;
+import com.jogamp.newt.opengl.GLWindow;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.util.FPSAnimator;
+
+public class Screen {
+
+    private static volatile Screen instance;
+    private GLWindow window;
+
+    private Screen() {
+        if (instance != null) {
+            throw new RuntimeException();
+        }
+
+        GLProfile.initSingleton();
+        GLCapabilities caps = new GLCapabilities(GLProfile.get(GLProfile.GL2));
+        window = GLWindow.create(caps);
+        window.setSize(800, 600);
+        window.setTitle("Snake Game");
+
+        window.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowDestroyNotify(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        window.addKeyListener(gameArea);
+
+        FPSAnimator animator = new FPSAnimator(window, 60);
+        animator.start();
+
+        window.setVisible(true);
+        window.display();
+        window.requestFocus();
+    }
+
+    public static void create() {
+        if (instance == null) {
+            synchronized (Screen.class) {
+                if (instance == null) {
+                    instance = new Screen();
+                }
+            }
+        }
+    }
+}
